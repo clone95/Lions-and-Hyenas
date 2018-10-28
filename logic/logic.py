7cf27049f2a7179e8941fe69c833e7128679ca81
jack <giac290595@gmail.com>
2018-10-28 20:21:13 +0000

    added functions to calculate ages and deatsh  from age need to fix a bug

--- logic/logic.py
+++ logic/logic.py
@@ -1,4 +1,5 @@
-from logic.park import *
+from logic.animals import *
+import random
 
 
 class AbstractFactory(object):
@@ -14,23 +15,17 @@ class Computer:
     def __init__(self):
         self.name = "survive_computer"
 
+# deaths
+
     def compute_deaths(self, park):
         park_deaths = dict()
 
         for animal_type in park.animals:
-            # print("______________________________")
-            # print("index:", animal_type, len(park.animals[animal_type]))
-
             park_deaths[animal_type] = self.die(park.animals[animal_type][0], park)
-
             if len(park.animals[animal_type]) <= -park_deaths[animal_type]:
                 park.animals[animal_type] = park.animals[animal_type][:1]
             else:
                 park.animals[animal_type] = park.animals[animal_type][:park_deaths[animal_type]]
-            # print(len(park.animals[animal_type]))
-            # print("--")
-            # print("morti: ", park_deaths[animal_type])
-            # print("index:", animal_type, len(park.animals[animal_type]))
         return park
 
     @staticmethod
@@ -55,23 +50,114 @@ class Computer:
 
         return int(deaths + puppy_shield)
 
+# births
+
     def compute_births(self, park):
+        park_births = dict()
+
+        for animal_type in park.animals:
+            species = animal_type[6:]
+            if animal_type[:5] == "adult":
+                park_births[animal_type] = self.generate_puppies(park.animals[animal_type][0], park, species)
+                call = "puppy_{}_factory.generate_exemplar()".format(species)
+
+                for i in range(0, park_births[animal_type]):
+                    park.animals["puppy_{}".format(species)].append(eval(call))
+
+        return park
+
+    @staticmethod
+    def generate_puppies(animal, park, species):
+        tot_erb = park.small_erb + park.big_erb
+        tot_carn = park.small_carn + park.big_carn
+        births = 0
+        adult_of_species = "adult_" + species
+        # how many puppies we can the populations generate?
+        if animal.diet == "meat":
+            if animal.size == "big":
+                births = len(park.animals[adult_of_species]) * 0.2 + random.randint(2, 20)
+            else:
+                births = len(park.animals[adult_of_species]) * 0.4 + random.randint(2, 40)
+        else:
+            if animal.size == "big":
+                births = len(park.animals[adult_of_species]) * 0.2 + random.randint(2, 20)
+            else:
+                births = len(park.animals[adult_of_species]) * 0.4 + random.randint(2, 40)
+
+        return int(births)
+
+# age
+    @staticmethod
+    def compute_age(park):
+
+        for animal in park.animals.keys():
+            species = animal[6:]
+            if animal[:5] == "adult":
+                for exemplar in park.animals[animal]:
+                    exemplar.age += 1
+                    if animal == "adult_lion":
+
+                        print(len(park.animals[animal]))
+                    if exemplar.age > exemplar.death and len(park.animals[animal]) > 1:
+                        park.animals[animal].remove(exemplar)
+                        print("a")
+            else:
+                for exemplar in park.animals[animal]:
+                    exemplar.age += 1
+
+                if exemplar.age > 1:
+
+                    if len(park.animals["puppy_" + animal[6:]]) > 1:
+
+                        park.animals["puppy_" + animal[6:]].remove(exemplar)
+                        print(len(park.animals["adult_" + animal[6:]]))
+                        call = "puppy_{}_factory.generate_exemplar()".format(species)
+                        park.animals["adult_" + animal[6:]].append(eval(call))
+                        print(len(park.animals["adult_" + animal[6:]]))
+
+
+
+
         pass
 
-    def compute_survivors(self, park):
-        pass
+
+# complete phase
 
     def compute_phase(self, park):
-        animals_after_deaths = self.compute_deaths(park)
-
-        # births = self.compute_births(park)
+        self.compute_deaths(park)
+        self.compute_births(park)
+        self.compute_age(park)
         # park = park + births
         # park = self.compute_survivors(park)
 
         #final_park.init_park()
         #print(animals_after_deaths)
-        return animals_after_deaths
 
+        return park
+
+
+def anim_gen(*args):
+    park = dict()
+    adults = ['adult_lion', 'adult_hyena', 'adult_zebra', 'adult_gnu',
+              'puppy_lion', 'puppy_hyena', 'puppy_zebra', 'puppy_gnu']
+    for arg in range(0, len(args)):
+        park[adults[arg]] = []
+        for i in range(0, args[arg]):
+            call = str(adults[arg]) + '_factory' + '.generate_exemplar()'
+            park[adults[arg]].append(eval(call))
+    return park
+
+
+# adults animal factory
+puppy_lion_factory = AbstractFactory(PuppyLion)
+puppy_hyena_factory = AbstractFactory(PuppyHyena)
+puppy_zebra_factory = AbstractFactory(PuppyZebra)
+puppy_gnu_factory = AbstractFactory(PuppyGnu)
+# puppies animal factory
+adult_lion_factory = AbstractFactory(AdultLion)
+adult_hyena_factory = AbstractFactory(AdultHyena)
+adult_zebra_factory = AbstractFactory(AdultZebra)
+adult_gnu_factory = AbstractFactory(AdultZebra)
 
 # def calculate_population(animal_list):
 #     types = set()
--- simulation.py
+++ simulation.py
@@ -3,7 +3,7 @@ from logic.animals import *
 from logic.logic import *
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.pyplot import figure
+
 # adults animal factory
 puppy_lion_factory = AbstractFactory(PuppyLion)
 puppy_hyena_factory = AbstractFactory(PuppyHyena)
@@ -16,19 +16,9 @@ adult_zebra_factory = AbstractFactory(AdultZebra)
 adult_gnu_factory = AbstractFactory(AdultZebra)
 
 
-def anim_gen(*args):
-    park = dict()
-    adults = ['adult_lion', 'adult_hyena', 'adult_zebra', 'adult_gnu',
-              'puppy_lion', 'puppy_hyena', 'puppy_zebra', 'puppy_gnu']
-    for arg in range(0, len(args)):
-        park[adults[arg]] = []
-        for i in range(0, args[arg]):
-            call = str(adults[arg]) + '_factory' + '.generate_exemplar()'
-            park[adults[arg]].append(eval(call))
-    return park
 
 
-animal_pool = anim_gen(60, 50, 200, 400, 8, 12, 20, 45)
+animal_pool = anim_gen(60, 50, 200, 200, 8, 12, 20, 45)
 park = Park(20, animal_pool, 2500)
 park.init_park()
 comp = Computer()
@@ -37,7 +27,7 @@ statistics = {}
 
 for animal in park.animals:
         statistics[animal] = []
-n = 10
+n = 100
 for age in range(0, n):
     for animal in park.animals:
         statistics[animal].append(len(park.animals[animal]))
@@ -47,16 +37,19 @@ for age in range(0, n):
 # set figsize -> big
 plt.rcParams["figure.figsize"] = [16, 9]
 # observations of the environment
-x = np.arange(10)
+x = np.arange(100)
 # plots every species
 for key in statistics.keys():
     plt.plot(x, statistics[key])
+
 # creates the legenda with all the species colours
 legenda = []
 for key in statistics.keys():
     legenda.append('y = {}'.format(key))
 # puts the legenda in the proper position and adjust the text dimension
-plt.legend(legenda, loc="upper_left", prop={'size': 12})
+plt.legend(legenda, loc="upper left", prop={'size': 12})
+
 # show the plot
-plt.show()
+# plt.show()
+
 
